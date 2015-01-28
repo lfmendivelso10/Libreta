3098b68a71628183ec80c067d0fd181817d700d6
Luis Felipe Mendivelso Osorio <lf.mendivelso10@uniandes.edu.co>
2015-01-28 11:41:15 -0500

    Update settings.py

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -19,7 +19,7 @@ import dj_database_url
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a0ioz4o%3c-2mbtpgnwvoza$+m2-e=q=ih6_&9o1syxx8902xi'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
